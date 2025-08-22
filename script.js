84055e1c0975fb2b24533e043bd68487db34e370
Christian <ctian5505@gmail.com>
2025-08-22 14:22:02 +0800

    Creating Product List and Editing Cart List

---
+++ script.js
@@ -0,0 +1,50 @@
+let cart = [];
+
+// Make each tile working
+document.querySelectorAll(".product-image").forEach(function (click) {
+  click.addEventListener("click", function () {
+    const product = click.parentElement;
+    const name = product.dataset.name;
+    const price = parseFloat(product.dataset.price);
+    const image = product.dataset.image;
+    console.log(name + " " + price + " " + image);
+    addToCart(name, price, image);
+  });
+});
+
+function addToCart(name, price, image) {
+  cart.push({ name, price, image });
+  console.log(cart);
+  updateCartDisplay();
+}
+
+function updateCartDisplay() {
+  const cartList = document.getElementById("cart");
+  const cartTotal = document.getElementById("total");
+  cartList.innerHTML = "";
+
+  let total = 0;
+
+  cart.forEach(function (item, index) {
+    total += item.price;
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+  <div class="cart-item">
+          <img
+            src="${item.image}"
+          />
+          <p>${item.name}</p>
+          <p>${item.price}</p>
+          <button class="removeBtn">Remove</button>
+        </div>
+  `;
+
+    cartList.appendChild(li);
+  });
+
+  cartTotal.innerHTML = `Total: P${total}`;
+}
+
+const removeFromCart = document.querySelectorAll(".removeBtn");
+removeFromCart.addEventListener("click", console.log("remove"));
